8ae1bdb23b3380b887e9abd78fc4a66f8e56c204
Madeline0110 <tprazeus0110@gmail.com>
2024-10-31 11:08:32 +1100

    Syntax fixes, initial test setup of ping pong functionality done

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const Discord = require("discord.js");
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 const config = require("./config.json");
 
 
-const client = new Discord.Client({intents: [GatewayIntentBits.Guilds]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+const prefix = "!";
+
+client.on("messageCreate", function(message) {
+    //console.log(message)
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+  
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === "ping") {
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+      }
+  });
 
 client.login(config.BOT_TOKEN);
