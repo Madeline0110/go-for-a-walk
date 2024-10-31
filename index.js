dce70b510abe031f7cdfd606f47ecaa6bceda361
Madeline0110 <tprazeus0110@gmail.com>
2024-10-31 12:55:39 +1100

    Added a template json object for userMsgCount file.
    Added handling for date in userMsgCount.json being out of date.
    
    Added function userExists(), to check if user currently exists for a given guild.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const config = require("./config.json");
-
+const fs = require('fs')
+const userMsgCount = require("./userMsgCount.json");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
@@ -10,8 +11,46 @@ client.once(Events.ClientReady, readyClient => {
 
 const prefix = "!";
 
+if (userMsgCount.date == ""){
+    var datetime = new Date();
+    userMsgCount.date = datetime.toDateString();
+    fs.writeFileSync('./userMsgCount.json',JSON.stringify(userMsgCount));
+}
+
+function userExists(targetGuildID,targetUserID){
+    /**
+     * @param {targetGuildID} string id of guild we are interested in.
+     * @param {targetUserID} string id of user we are interested in.
+     * @returns {state}, boolean state representing whether or not target user exists in target guild msg logs.
+     */
+
+    let state = false;
+
+    if (userMsgCount.guilds.hasOwnProperty(targetGuildID)){
+        guildUsers = userMsgCount.guilds[targetGuildID].users;
+        for (let i = 0; i < guildUsers.length; i++){
+            if (guildUsers[i].userID === targetUserID){
+                state = true;
+            }
+        }
+    }
+
+    return state;
+}
+
+
+
 client.on("messageCreate", function(message) {
-    //console.log(message)
+
+    //check if we need to update current date in json.
+    let currDate = new Date().toDateString();
+    if (userMsgCount.date.split(" ")[2] !== currDate.split(" ")[2]){
+        console.log(`Updating date from ${userMsgCount.date} to ${currDate}`)
+        userMsgCount.date = currDate;
+        fs.writeFileSync('./userMsgCount.json',JSON.stringify(userMsgCount));
+    }
+
+
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
   
@@ -22,7 +61,10 @@ client.on("messageCreate", function(message) {
     if (command === "ping") {
         const timeTaken = Date.now() - message.createdTimestamp;
         message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-      }
+    }
+
+    
+
   });
 
 client.login(config.BOT_TOKEN);
