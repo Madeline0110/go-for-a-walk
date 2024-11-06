31617738ed5a8a618ef6c6d606a6b515526dbf82
Madeline0110 <tprazeus0110@gmail.com>
2024-11-06 11:54:59 +1100

    Added functions, guildExists and addUser. Implemented !help command and general syntax for rest to be implemented. Added ability to increment msg count for existing user.

--- index.js
+++ index.js
@@ -10,11 +10,23 @@ client.once(Events.ClientReady, readyClient => {
 });
 
 const prefix = "!";
+let msgLimit = 100;
+let muteState = false;
+let adminRole = '';
 
 if (userMsgCount.date == ""){
     var datetime = new Date();
     userMsgCount.date = datetime.toDateString();
     fs.writeFileSync('./userMsgCount.json',JSON.stringify(userMsgCount));
+}
+
+function guildExists(targetGuildID){
+    /**
+     * @param {targetGuildID} string id of guild we are interested in.
+     * @returns true or false, dependingon if guild exists
+     */
+
+    return userMsgCount.guilds.hasOwnProperty(targetGuildID);
 }
 
 function userExists(targetGuildID,targetUserID){
@@ -38,11 +50,76 @@ function userExists(targetGuildID,targetUserID){
     return state;
 }
 
+function addUser(targetGuildID,targetUserID,targetUsername){
+    /** Add a new user to the users array for a given guild
+     * 
+     * @param {targetGuildID} string id of guild we are interested in.
+     * @param {targetUserID} string id of user we are interested in.
+     * @param {targetUsername} string username of user we are interested in.
+     */
+    userMsgCount.guilds[targetGuildID].users.push({ "userID": targetUserID, "username": targetUsername, "msgCount": 1 });
+    fs.writeFileSync('./userMsgCount.json',JSON.stringify(userMsgCount));
+    console.log(`Added new user ID: ${targetUserID} Name: ${targetUsername}`)
+}
 
+function incUserMsgCount(targetGuildID,targetUserID){
+    /** Increment the msgCount attribute for a given user
+     * 
+     * @param {targetGuildID} string id of guild we are interested in.
+     * @param {targetUserID} string id of user we are interested in.
+     */
+
+    const usersArr = userMsgCount.guilds[targetGuildID].users
+    for (let i = 0; i < usersArr.length; i++){
+        if (usersArr[i].userID === targetUserID){
+            usersArr[i].msgCount = usersArr[i].msgCount + 1;
+
+            userMsgCount.guilds[targetGuildID].users = usersArr;
+            fs.writeFileSync('./userMsgCount.json',JSON.stringify(userMsgCount));
+            return;
+        }
+    }
+}
 
 client.on("messageCreate", function(message) {
+    
+    //dont want to log bot messages
+    if (message.author.bot) return;
 
-    //check if we need to update current date in json.
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+    const userRoles = message.member.roles.cache.map(role => role.name);
+
+
+    if (adminRole === '' || userRoles.includes(adminRole)){
+        if (command == "help"){
+            message.reply("There currently exists the following commands:\n* !help - returns list of all commands.\n* !setAdmin - Set the admin role that can call ! commands.\n* !setMsgLimit - Set the limit for which further messages from a user will result in mute / 'take a walk msg'.\n* !toggleMute - Turn muting of users that cross message limit threshold on or off.\n* !setMuteLength - Set mute duration (minutes).\n* !unmute {username/id} - Given a valid username or id input, unmute this user.");
+            return;
+        }else if (command == "setadmin"){
+            
+    
+        }else if (command == "setmsglimit"){
+    
+        }else if (command == "togglemute"){
+    
+        }else if (command == "setMuteLength"){
+    
+        }else if (command == "unmute"){
+        
+        }else if (command == "setMuteLength"){
+    
+        }
+    }
+
+
+
+    // console.log(message)
+    // console.log(message.author.id)
+    // console.log(message.author.username)
+    console.log(message.member.roles.cache.map(role => role.name))
+
+    // check if we need to update current date in json.
     let currDate = new Date().toDateString();
     if (userMsgCount.date.split(" ")[2] !== currDate.split(" ")[2]){
         console.log(`Updating date from ${userMsgCount.date} to ${currDate}`)
@@ -50,13 +127,28 @@ client.on("messageCreate", function(message) {
         fs.writeFileSync('./userMsgCount.json',JSON.stringify(userMsgCount));
     }
 
+    const targetGuildID = message.guildId;
+    const targetUserID = message.author.id;
+    const targetUsername = message.author.username;
 
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
+    // check if guild for msg doesnt exist
+    if (!guildExists(targetGuildID)){
+        userMsgCount.guilds[targetGuildID] = {"users":[{ "userID": targetUserID, "username": targetUsername, "msgCount": 1 }]};
+        fs.writeFileSync('./userMsgCount.json',JSON.stringify(userMsgCount));
+    }
+    // check if user exists in a given guild
+    else if (!userExists(targetGuildID, targetUserID)){
+        addUser(targetGuildID,targetUserID,targetUsername);
+    } else{
+        //if user does exist then increment their msgCount counter
+        incUserMsgCount(targetGuildID,targetUserID);
+    }
+
+
+
+    
   
-    const commandBody = message.content.slice(prefix.length);
-    const args = commandBody.split(' ');
-    const command = args.shift().toLowerCase();
+
 
     if (command === "ping") {
         const timeTaken = Date.now() - message.createdTimestamp;
