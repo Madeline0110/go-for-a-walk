2ee8defb0b40fe69907b90dd85231c4775b290f7
Madeline0110 <tprazeus0110@gmail.com>
2024-11-12 12:57:09 +1100

    Added functionality for the following commands: setmsglimit, togglemute, setmutelength, setmsg

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ client.once(Events.ClientReady, readyClient => {
 });
 
 const prefix = "!";
-let msgLimit = 100;
-let muteState = false;
-let adminRole = '';
+
 
 if (userMsgCount.date == ""){
     var datetime = new Date();
@@ -86,29 +84,70 @@ client.on("messageCreate", function(message) {
     //dont want to log bot messages
     if (message.author.bot) return;
 
+    // format input string assuming it is a command
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
     const userRoles = message.member.roles.cache.map(role => role.name);
 
+    const targetGuildID = message.guildId;
+    const targetUserID = message.author.id;
+    const targetUsername = message.author.username;
+    const guildAdminRole = userMsgCount.guilds[targetGuildID].adminRole;
 
-    if (adminRole === '' || userRoles.includes(adminRole)){
+    //limit users who can call commands
+    if (guildAdminRole === '' || userRoles.includes(guildAdminRole)){
         if (command == "help"){
             message.reply("There currently exists the following commands:\n* !help - returns list of all commands.\n* !setAdmin - Set the admin role that can call ! commands.\n* !setMsgLimit - Set the limit for which further messages from a user will result in mute / 'take a walk msg'.\n* !toggleMute - Turn muting of users that cross message limit threshold on or off.\n* !setMuteLength - Set mute duration (minutes).\n* !unmute {username/id} - Given a valid username or id input, unmute this user.");
             return;
         }else if (command == "setadmin"){
-            
+            userMsgCount.guilds[targetGuildID].adminRole = args[0];
+            fs.writeFileSync('./userMsgCount.json',JSON.stringify(userMsgCount));
+            message.reply(`Set admin role to ${args[0]}`);
+            return;
     
         }else if (command == "setmsglimit"){
-    
+            if (!Number.isInteger(Number(args[0])) || Number(args[0]) <= 0){
+                message.reply("Invalid input, must be a valid Integer > 0.")
+                return;
+            }
+            userMsgCount.guilds[targetGuildID].msgLimit = Number(args[0]);
+            fs.writeFileSync('./userMsgCount.json',JSON.stringify(userMsgCount));
+            message.reply(`Set msgLimit to ${args[0]}`);
+            return;
+
         }else if (command == "togglemute"){
+            
+            //get current mute state, and negate it.
+            let currState = userMsgCount.guilds[targetGuildID].muteState;
+            if (currState){
+                userMsgCount.guilds[targetGuildID].muteState = false;
+            } else {
+                userMsgCount.guilds[targetGuildID].muteState = true;
+            }
+
+            fs.writeFileSync('./userMsgCount.json',JSON.stringify(userMsgCount));
+            message.reply(`Set mute state to ${!currState}.`);
+            return;
     
-        }else if (command == "setMuteLength"){
+        }else if (command == "setmutelength"){
+            // ensure input is valid integer > 0
+            if (!Number.isInteger(Number(args[0])) || Number(args[0]) <= 0){
+                message.reply("Invalid input, must be a valid Integer > 0.")
+                return;
+            }
+            userMsgCount.guilds[targetGuildID].muteLength = Number(args[0]);
+            fs.writeFileSync('./userMsgCount.json',JSON.stringify(userMsgCount));
+            message.reply(`Set mute length to ${args[0]}`);
+            return;
     
         }else if (command == "unmute"){
         
-        }else if (command == "setMuteLength"){
-    
+        }else if (command == "setmsg"){
+            userMsgCount.guilds[targetGuildID].touchGrassMSG = args[0];
+            fs.writeFileSync('./userMsgCount.json',JSON.stringify(userMsgCount));
+            message.reply(`Set custom message to ${args[0]}`);
+            return;
         }
     }
 
@@ -117,7 +156,6 @@ client.on("messageCreate", function(message) {
     // console.log(message)
     // console.log(message.author.id)
     // console.log(message.author.username)
-    console.log(message.member.roles.cache.map(role => role.name))
 
     // check if we need to update current date in json.
     let currDate = new Date().toDateString();
@@ -127,13 +165,16 @@ client.on("messageCreate", function(message) {
         fs.writeFileSync('./userMsgCount.json',JSON.stringify(userMsgCount));
     }
 
-    const targetGuildID = message.guildId;
-    const targetUserID = message.author.id;
-    const targetUsername = message.author.username;
-
     // check if guild for msg doesnt exist
     if (!guildExists(targetGuildID)){
-        userMsgCount.guilds[targetGuildID] = {"users":[{ "userID": targetUserID, "username": targetUsername, "msgCount": 1 }]};
+        userMsgCount.guilds[targetGuildID] = {
+            "adminRole": "",
+            "msgLimit": 100,
+            "muteState": false,
+            "muteLength": 30,
+            "touchGrassMSG": "Go for a walk.",
+            "users":[{ "userID": targetUserID, "username": targetUsername, "msgCount": 1 }]
+        };
         fs.writeFileSync('./userMsgCount.json',JSON.stringify(userMsgCount));
     }
     // check if user exists in a given guild
@@ -145,17 +186,6 @@ client.on("messageCreate", function(message) {
     }
 
 
-
-    
-  
-
-
-    if (command === "ping") {
-        const timeTaken = Date.now() - message.createdTimestamp;
-        message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-    }
-
-    
 
   });
 
